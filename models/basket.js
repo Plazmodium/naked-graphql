a4dd551136923776b2fee9946e5bfeae62975cf9
Gabor Racz <gabzracz@icloud.com>
2021-05-16 22:18:39 +0200

    changes

--- models/basket.js
+++ models/basket.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const basketSchema = new Schema({
-	id: Number,
 	startDate: String,
 	items: Array,
 	totalPremium: Number,
--- models/item.js
+++ models/item.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-	id: Number,
 	title: String,
 	subtitle: String,
 	itemType: String,
